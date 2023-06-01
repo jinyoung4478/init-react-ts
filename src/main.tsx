af6faebe436fed0207723863583e75f9f223161b
김진영 <skswls0@daum.net>
2023-06-01 21:43:51 +0900

    heroicons 라이브러리 추가 및 material-tailwind 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import { ThemeProvider } from '@material-tailwind/react';
 import router from '@/Router';
-// import '@/styles/globals.css';
+import '@/styles/globals.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
    <React.StrictMode>
-      <RouterProvider router={router} />
+      <ThemeProvider>
+         <RouterProvider router={router} />
+      </ThemeProvider>
    </React.StrictMode>,
 );
