e158e375b116cf873ff1561b2cb583b47d47394a
김진영 <skswls0@daum.net>
2024-01-23 23:23:38 +0900

    Develop (#6)
    * [feat] Link 컴포넌트 추가 (#5)
    
    Co-authored-by: 김진영 <jinyoung4478@daum.net>
    
    * [test] Test commit to check squash merge - 1
    
    * [test] Test commit to check squash merge - 2
    
    ---------
    
    Co-authored-by: 김진영 <jinyoung4478@daum.net>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,14 @@
 import { Tooltip } from '@/components/Tooltip';
+import { Link } from '@/components/Link';
 
 export default function Home() {
   return (
     <div>
+      <h1>TEST PAGE</h1>
       <Tooltip>
         <div className="absolute -bottom-8 left-0 bg-blue-200">This is tooltip</div>
       </Tooltip>
+      <Link to={'/test'} state={{ dfs: 'fads' }} label="Go to test page" />
     </div>
   );
 }
