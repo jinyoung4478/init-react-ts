44fe533e8cdb9c3d41a7d3611b6d74c70bc42762
김진영 <skswls0@daum.net>
2024-01-23 23:20:44 +0900

    [feat] Link 컴포넌트 추가 (#5)
    Co-authored-by: 김진영 <jinyoung4478@daum.net>

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { Tooltip } from '@/components/Tooltip';
+import { Link } from '@/components/Link';
 
 export default function Home() {
   return (
@@ -6,6 +7,7 @@ export default function Home() {
       <Tooltip>
         <div className="absolute -bottom-8 left-0 bg-blue-200">This is tooltip</div>
       </Tooltip>
+      <Link to={'/test'} state={{ dfs: 'fads' }} label="Go to test page" />
     </div>
   );
 }
